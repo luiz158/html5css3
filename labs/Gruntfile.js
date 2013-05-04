c60e2e33de125af9571332040cee00bc756daf60
Ricardo Borillo <borillo@gmail.com>
2013-05-04 20:05:41 +0200

    Grunt config for livereload

---
+++ labs/Gruntfile.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var path = require('path');
+var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
+
+var folderMount = function folderMount(connect, point) {
+  return connect.static(path.resolve(point));
+};
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    livereload: {
+      port: 35729
+    },
+    connect: {
+      livereload: {
+        options: {
+          port: 9001,
+          middleware: function(connect, options) {
+            return [lrSnippet, folderMount(connect, options.base)]
+          }
+        }
+      }
+    },
+    regarde: {
+      txt: {
+        files: ['**/*.html', '**/*.css'],
+        tasks: ['livereload']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-regarde');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-livereload');
+
+  grunt.registerTask('default', ['livereload-start', 'connect', 'regarde']);
+};
